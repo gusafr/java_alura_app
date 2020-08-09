106d46ebbbd9a4e10d1d716be6a003d8e39dce57
Gustavo Freitas <gusafr@gmail.com>
2020-08-09 10:11:19 -0300

    Initial commit

---
+++ src/main/java/TestaPontoFlutuante.java
@@ -0,0 +1,9 @@
+public class TestaPontoFlutuante {
+
+    public static void main(String[] args) {
+
+        double salario = 1250.05;
+        System.out.println("Salario " + salario);
+    }
+
+}
---
+++ src/main/java/TestaValores.java
@@ -0,0 +1,7 @@
+public class TestaValores {
+
+    public static void main(String[] args) {
+        int teste = 0;
+    }
+
+}
